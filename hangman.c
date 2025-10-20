a54512f86161a3dea582d5b1e43a259e73778586
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-20 19:43:06 +0000

    Co-authored-by: No-One117 <No-One117@users.noreply.github.com> Co-authored-by: ducks4444 <ducks4444@users.noreply.github.com> Co-authored-by: KenGi07 <KenGi07@users.noreply.github.com>

---
+++ hangman.c
@@ -0,0 +1,15 @@
+// 6th Hangman game  AK,KS,LL,AT 
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    list_of_words= ["ocean","water","kenji","ducky","lukas","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",]
+    printf("wwelcome to hangman,")
+    
+
+
+
+    // AK if you lose and keeping tack of hangman parts
+    
+}
+list_of_words= ["ocean","water","kenji","ducky","luas","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",]
