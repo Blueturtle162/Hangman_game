48b703cf4b4bf47f3a8020f28b2ccb8f232966ee
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-10-23 19:43:06 +0000

    Co-authored-by: ducks4444 <ducks4444@users.noreply.github.com> Co-authored-by: KenGi07 <KenGi07@users.noreply.github.com> Co-authored-by: No-One117 <No-One117@users.noreply.github.com>

--- han_man.c
+++ han_man.c
@@ -7,15 +7,14 @@
 #include <time.h>
 
 // KS made the list and random of the code
-const char* words[] = {"ocean","water","kenji","ducky","kayze","eliza","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread"};
+const char* words[] = {"ocean","water","kenji","ducky","kayze","eliza","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread","among"};
 // AK added extra instructions
 
 int num_words = sizeof(words) / sizeof(words[0]);
 char guessed_letters[26] = {0};
 int guessed_count = 0;
 
-
-
+//KS added user input
 int guess(const char* word) {
     char user_input[100];
     printf("Guess a letter: ");
@@ -30,7 +29,7 @@ int guess(const char* word) {
             printf("You already guessed that letter!\n");
             return 0;
         }
-    }
+    } //AK built hangman
     if (strcmp(user_input, word) == 0) {
         printf("Horray! You guessed the word! Rerun the code to play again.\n");
         exit(0);
@@ -50,7 +49,7 @@ int guess(const char* word) {
     }
 }
 
-
+// KS random and instructions
 int main() {
     srand((unsigned) time(NULL));
     const char* word = words[rand() % num_words];
@@ -62,13 +61,15 @@ int main() {
     int strikes = 0;
 
 
-    printf("Hi welcome to hangman! This game consists of an unkown five letter word. First, begin to assemble the word by guessing one letter at a time make sure to only enter lowercase letters with no extra spaces. Second, if you guess a letter that is not in the word, a strike will appear. If you gain 8 strikes, you're out. Lastly, if you think you know the word, you can guess the whole word at once. Try to fill in the whole word with minimal strikes. Have fun playing!\n"); //AK print statement
+    printf("Hi welcome to hangman! This game consists of an unkown five letter word. First, begin to assemble the word by guessing one letter at a time make sure to only enter lowercase letters with no extra spaces. Second, if you guess a letter that is not in the word, a strike will appear. If you gain 6 strikes, you're out. Lastly, if you think you know the word, you can guess the whole word at once. Try to fill in the whole word with minimal strikes. Have fun playing!\n"); //AK print statement
 
     
     char output_list[6] = {0};
 
-// Lucas until 88
-    (strikes < 8); {
+
+
+           // Lucas \/
+     while (strikes < 7) {
         strikes += guess(word);
         guessed_count = strlen(guessed_letters);
         printf("Strikes: %d\n", strikes);
@@ -90,20 +91,29 @@ int main() {
         output_list[strlen(word)] = '\0';
         printf("%s\n", output_list);
         if (strcmp(word, output_list) == 0) {
-            printf("You guessed the word! Run the code again to play.\n");
-              break;
+            printf("Hooray! You guessed the word! Rerun the code to play again.\n");
+            break;
 
 
-            } else {
+        } else {
             continue;
-            
-    
-
-           
+        }
+    }
 
 
-            else (strikes == 0);
-            printf("You lost! The word was %s.\n", word);
-            return 0;
+    if (strikes == 7) {
+        printf("You lost! The word was %s.\n", word);
     }
 }
+
+
+
+
+//  0
+// /|\
+//  |
+// / \
+
+
+
+
--- hangman.c
+++ hangman.c
@@ -1,15 +1,43 @@
 // 6th Hangman game  AK,KS,LL,AT 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
+    //KS list and randomizer
 int main(void){
-    list_of_words= ["ocean","water","kenji","ducky","lukas","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",]
-    printf("wwelcome to hangman,")
+    char words[]= {"ocean","water","kenji","ducky","lukas","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",} ;
+    srand(time(NULL));
+    int num = rand() %45;
+    printf("%c\n",words[num]);
+    printf("Hi welcome to hangman!\nThis game consists of an unkown five letter word. First, begin to assemble the word by guessing one letter at a time. Second, if you guess a letter that is not in the word, a strike will appear. If you gain 6 strikes, you're out. Lastly, if you think you know the word, you can guess the whole word at once. Try to fill in the whole word with minimal strikes. Have fun playing!");
     
-
-
+    //AT if you guess it correctly 
+    if %default == "";
+    printf("guess a valid letter.");
+    return 0;
+    else if %default == char word
+        exit("Horray! you guessed the word rerun to play again.");
+        return 0;
+    else if scrl (%default) >1;
+    
 
     // AK if you lose and keeping tack of hangman parts
+    int incorrect_hangman_guesses;
+    if (incorrect_hangman_guesses ==6){
+        printf ("That is incorrect you have 0 worng gueses left. You lose.");
+    }else if (incorrect_hangman_guesses == 5){
+        printf("That is incorrect. You have ")
+    }
+
+    
+
     
 }
-list_of_words= ["ocean","water","kenji","ducky","luas","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",]
+
+
+
+
+
+
+
